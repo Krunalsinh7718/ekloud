6f36114c5f65320d096a5ce25d927193db9934c4
Krunalsinh <krunal5281@gmail.com>
2026-02-08 00:52:50 +0530

    changes

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     // BEGIN: sliders ----------------------------
+
+    const arrowPath = "M0.498047 9.00336C-0.166016 9.66742 -0.166016 10.7459 0.498047 11.4099L8.99805 19.9099C9.66211 20.574 10.7405 20.574 11.4046 19.9099C12.0687 19.2459 12.0687 18.1674 11.4046 17.5034L5.80523 11.904H25.4987C26.439 11.904 27.1987 11.1443 27.1987 10.204C27.1987 9.26367 26.439 8.50398 25.4987 8.50398H5.80523L11.4046 2.90461C12.0687 2.24055 12.0687 1.16211 11.4046 0.498047C10.7405 -0.166016 9.66211 -0.166016 8.99805 0.498047L0.498047 8.99805V9.00336Z"
     const logoSlider = document.querySelectorAll(".logo-slider");
 
     logoSlider.forEach(e => {
@@ -24,34 +26,67 @@ document.addEventListener("DOMContentLoaded", () => {
             type: 'loop',
             drag: 'free',
             focus: 'center',
-            // autoWidth: true,
-            // arrows: false,
             pagination: false,
-            // gap: 20,
-            arrowPath: "M0.498047 9.00336C-0.166016 9.66742 -0.166016 10.7459 0.498047 11.4099L8.99805 19.9099C9.66211 20.574 10.7405 20.574 11.4046 19.9099C12.0687 19.2459 12.0687 18.1674 11.4046 17.5034L5.80523 11.904H25.4987C26.439 11.904 27.1987 11.1443 27.1987 10.204C27.1987 9.26367 26.439 8.50398 25.4987 8.50398H5.80523L11.4046 2.90461C12.0687 2.24055 12.0687 1.16211 11.4046 0.498047C10.7405 -0.166016 9.66211 -0.166016 8.99805 0.498047L0.498047 8.99805V9.00336Z"
+            arrowPath: arrowPath
         });
 
         splide.mount();
     })
 
     const progressSlider = document.querySelectorAll(".progress-slider");
-    console.log(progressSlider);
     progressSlider.forEach(e => {
-        const bar = e.querySelector( '.my-slider-progress' );
-        
-        const splide = new Splide(e,{
-            pagination: false
+        const bar = e.querySelector('.my-slider-progress-bar');
+
+        const splide = new Splide(e, {
+            pagination: false,
+            arrowPath: arrowPath
+
         });
 
-        splide.on( 'mounted move', function () {
-        let end  = splide.Components.Controller.getEnd() + 1;
-        let rate = Math.min( ( splide.index + 1 ) / end, 1 );
-        bar.style.width = String( 100 * rate ) + '%';
-    } );
+        splide.on('mounted move', function () {
+            let end = splide.Components.Controller.getEnd() + 1;
+            let rate = Math.min((splide.index + 1) / end, 1);
+            bar.style.width = String(100 * rate) + '%';
+        });
 
         splide.mount();
     })
 
+    const autoWidthSlider = document.querySelectorAll(".auto-width-slider");
+    const slidePrevBtn = document.querySelector('.testo-slide-previous');
+    const slideNextBtn = document.querySelector('.testo-slide-next');
+    autoWidthSlider.forEach(e => {
+        const splide = new Splide(e, {
+            type: 'loop',
+            drag: 'free',
+            pagination: false,
+            arrows: false,
+            autoWidth: true,
+            gap: 40,
+            autoplay: true,
+            breakpoints: {
+                768: {
+                    gap: 15,
+                },
+            }
+        });
+
+        splide.mount();
+
+        if (slidePrevBtn) {
+            console.log("1");
+            slidePrevBtn.addEventListener('click', e => {
+
+                splide.go('+1')
+            })
+        }
+        if (slideNextBtn) {
+            slideNextBtn.addEventListener('click', e => {
+                splide.go('-1')
+            })
+        }
+
+    })
 
     // END: sliders ----------------------------
     // BEGIN: nav link click ----------------------------
